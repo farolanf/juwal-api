542e2609237f1a7138cf654ce4b131812d0c9c4a
Farolan Faisal <farolanfaisal@gmail.com>
2019-10-31 21:28:19 +0800

    add data importer

---
+++ api/devel/controllers/Devel.js
@@ -0,0 +1,41 @@
+'use strict';
+const fs = require('fs')
+const _ = require('lodash')
+const yaml = require('yaml')
+
+/**
+ * Read the documentation () to implement custom controller functions
+ */
+
+module.exports = {
+  async resetCategories (ctx) {
+    const content = fs.readFileSync('data/categories.yml')
+    const categories = yaml.parse(content.toString())
+    await strapi.services.category.delete({})
+    _.each(categories, async (children, name) => {
+      const parent = await strapi.services.category.create({ name })
+      _.each(children, async childName => {
+        await strapi.services.category.create({ name: childName, parent: parent._id })
+      })
+    })
+    ctx.send({
+      count: await strapi.services.category.count()
+    })
+  },
+
+  async resetProvinsi (ctx) {
+    const content = fs.readFileSync('data/provinsi.yml')
+    const provinsis = yaml.parse(content.toString())
+    await strapi.services.provinsi.delete({})
+    await strapi.services.kabupaten.delete({})
+    _.each(provinsis, async (kabupatens, provinsiName) => {
+      const provinsi = await strapi.services.provinsi.create({ name: provinsiName })
+      _.each(kabupatens, async kabupatenName => {
+        await strapi.services.kabupaten.create({ name: kabupatenName, provinsi: provinsi._id })
+      })
+    })
+    ctx.send({
+      count: await strapi.services.category.count()
+    })
+  }
+};
