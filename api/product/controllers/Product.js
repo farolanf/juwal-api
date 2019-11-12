fb67898fcf58c1a1c8278465777db07e83a9e494
Farolan Faisal <farolanfaisal@gmail.com>
2019-11-12 21:28:23 +0800

    update relation

--- api/product/controllers/Product.js
+++ api/product/controllers/Product.js
@@ -1,3 +1,20 @@
 'use strict';
 
-module.exports = {}
+const _ = require('lodash')
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils')
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      if (_.castArray(files.images).length > strapi.config.maxProductImages) {
+        return ctx.badRequest()
+      }
+      entity = await strapi.services.product.create(data, { files });
+    } else {
+      entity = await strapi.services.product.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.product });
+  }
+}
