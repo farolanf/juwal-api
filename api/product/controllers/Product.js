315e0a5994b399d27e4143a1fa25160a6b314781
Farolan Faisal <farolanfaisal@gmail.com>
2019-11-15 22:12:35 +0800

    add product owner find check

--- api/product/controllers/Product.js
+++ api/product/controllers/Product.js
@@ -4,6 +4,27 @@ const _ = require('lodash')
 const { parseMultipartData, sanitizeEntity } = require('strapi-utils')
 
 module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.product.search(ctx.query);
+    } else {
+      const paramNames = _.keys(ctx.query)
+      if (_.find(paramNames, name => _.startsWith(name, 'owner_'))) {
+        return ctx.badRequest()
+      }
+      if (_.find(paramNames, 'owner')) {
+        // restrict find by owner only to the owner
+        if (!ctx.state.user || ctx.query.owner !== ctx.state.user._id) {
+          return ctx.badRequest()
+        }
+      }
+      entities = await strapi.services.product.find(ctx.query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.product }));
+  },
+
   async create(ctx) {
     let entity;
     if (ctx.is('multipart')) {
