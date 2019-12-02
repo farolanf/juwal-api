3541ffdfe59a547ea8093fa4d5bc782f7e1d6d0f
Farolan Faisal <farolanfaisal@gmail.com>
2019-12-02 08:12:37 +0800

    populate nested categories

--- api/category/controllers/Category.js
+++ api/category/controllers/Category.js
@@ -1,8 +1,23 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/guides/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.category.search(ctx.query);
+    } else {
+      entities = await strapi.services.category.find(ctx.query, {
+        path: 'categories',
+        populate: 'categories'
+      });
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.category }));
+  }
+};
